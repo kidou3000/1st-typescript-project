1fc7fd790d8b7c550e12f8014cac0835a4b39777
kidou3000 <kidou3000@gmail.com>
2024-10-21 23:56:53 +0900

    main2 ok

--- main2.ts
+++ main2.ts
@@ -1,9 +1,16 @@
-console.log("Hellow World");
-
-function startTimer2(duration:number){
+function startTimer2(duration: number) {
     let timer=duration;
-    const intervalId=setInterval(()=>{},1000);
+    const intervalId=setInterval(()=>{
+        const minutes=Math.floor(timer/60);
+        const seconds=timer%60;
+        console.log(`${minutes}:${seconds}`);
+
+        if(--timer<0){
+            clearInterval(intervalId);
+            console.log("timer is end");
+        };
+    },1000);
 }
 
 
-startTimer2(5*60);
+startTimer2(1*3);
