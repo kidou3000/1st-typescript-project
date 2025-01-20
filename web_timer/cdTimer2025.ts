6f6062ed09bb4ad1c57578aacbd7b0a1991c059a
kidou3000 <kidou3000@gmail.com>
2025-01-20 20:57:31 +0900

    202501

---
+++ web_timer/cdTimer2025.ts
@@ -0,0 +1,29 @@
+// タイマー関数
+// function countdown(seconds: number): void {
+//     let remaining = seconds;
+  
+//     const intervalId = setInterval(() => {
+//       console.log(remaining);
+//       remaining--;
+  
+//       if (remaining < 0) {
+//         clearInterval(intervalId);
+//         console.log("Time's up!");
+//       }
+//     }, 1000); // 1秒ごとに実行
+//   }
+  
+//   // 使い方
+//   countdown(10); // 10秒のカウントダウン
+  
+function cowuntdown(seconds: number):void{
+  let remaining=seconds;
+
+  const intervalId=setInterval(()=>{
+    console.log("write!");
+
+  }
+,1000);
+}
+
+cowuntdown(10);
